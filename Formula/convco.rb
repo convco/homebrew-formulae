0fd7965bdb91dab7ad723172ec479e4731b6f73b
hdevalke <2261239+hdevalke@users.noreply.github.com>
2021-02-23 20:38:28 +0100

    fix: remove bottle

--- Formula/convco.rb
+++ Formula/convco.rb
@@ -11,13 +11,6 @@ class Convco < Formula
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
-  bottle do
-    root_url "https://github.com/convco/convco/releases/download/v0.3.2"
-    cellar :any_skip_relocation
-    sha256 "a0c3abc4e68481cb2ceb7e4992a8c7d6957b9fd266cc5beb217500d8088059a4" => :x86_64_linux
-    sha256 "7786f32a696630e9fa326b2404f741e199c51b8fe995a4d033c21aea7c20bd40" => :catalina
-  end
-
   depends_on "rust" => [:optional, :build]
 
   def install
