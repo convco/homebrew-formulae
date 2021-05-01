256468c1e7f212bb174d323bbbc071c299e38adf
hdevalke <2261239+hdevalke@users.noreply.github.com>
2021-05-01 10:24:08 +0000

    convco: add 0.3.5 bottle.

--- Formula/convco.rb
+++ Formula/convco.rb
@@ -11,6 +11,12 @@ class Convco < Formula
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
+  bottle do
+    root_url "https://github.com/convco/homebrew-formulae/releases/download/convco-0.3.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "93e4a59b728fb08cdd80a2324bf9b041cd93fe7617a1869a15706ec0fe393556"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "11c0bc2da05e27a5baf742c0c5e50c24a9790e13ba9a47db4caed24a9a3c2f01"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
